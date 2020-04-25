3ebdf3b22f300d8cbfed25a2e0b731baf7757d85
Nitwel <nitwel@arcor.de>
2020-04-26 01:38:21 +0200

    fix file import

--- classes/deck_loader.py
+++ classes/deck_loader.py
@@ -1,11 +1,12 @@
 import json
+from os import path
 
 def load_decks(decks, lang):
     black_cards = []
     white_cards = []
 
     for deck in decks:
-        f = open("../decks/{0}/{1}.txt".format(lang, deck), "r")
+        f = open(path.dirname(__file__) + "../decks/{0}/{1}.txt".format(lang, deck), "r")
         data = json.loads(f.read())
         black_cards += data['blackCards']
         white_cards += data['whiteCards'] 
